0ac3e7700de382b4899f424f41b3e8ff55301101
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-07-12 02:24:21 -0300

    Arquivo criado para o Navbar

---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <h3><a href="/">Home</a></h3>
+      <h3><a href="#um-pouquinho-sobre-mim">Um pouquinho sobre mim</a></h3>
+      <h3><a href="#areas-em-que-posso-ajudar">Áreas em que posso ajudar</a></h3>
+      <h3><a href="#como-sera-a-nossa-jornada">Como será a nossa jornada</a></h3>
+      <h3><a href="#vamos-conversar">Vamos conversar?</a></h3>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/Components/Navbar";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,16 +7,4 @@ export default function Home() {
     <main className={styles.main}>
     </main>
   </>);
-}
-
-function Navbar() {
-  return (
-    <nav className={styles.navbar}>
-      <h3><a href="/">Home</a></h3>
-      <h3><a href="#um-pouquinho-sobre-mim">Um pouquinho sobre mim</a></h3>
-      <h3><a href="#areas-em-que-posso-ajudar">Áreas em que posso ajudar</a></h3>
-      <h3><a href="#como-sera-a-nossa-jornada">Como será a nossa jornada</a></h3>
-      <h3><a href="#vamos-conversar">Vamos conversar?</a></h3>
-    </nav>
-  );
 }
