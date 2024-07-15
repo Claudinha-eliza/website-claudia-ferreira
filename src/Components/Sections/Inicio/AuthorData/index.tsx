adcae6b008b8786def93d1e3c7807b9c9e30f807
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-07-15 16:48:09 -0300

    section 1 first version finish

--- src/Components/Sections/Inicio/AuthorData/index.tsx
+++ src/Components/Sections/Inicio/AuthorData/index.tsx
@@ -8,17 +8,17 @@ export function AuthorData() {
         src="/logo1.png"
         width={741}
         height={218}
-        alt="Picture of author's name"
+        alt="Imagem contendo o nome Claudia, sua profissão e o número do CRP"
         className={styles.logo}
       />
       <div className={styles.authorCareer}>
-        <h2>psicóloga I psicanalista</h2>
-        <h3>CRP 04/42649</h3>
+        <h1>psicóloga I psicanalista</h1>
+        <h2>CRP 04/42649</h2>
         <Image
           src="/decoration1.png"
           width={425}
           height={65}
-          alt="background decoration"
+          alt="decoração de fundo 1"
           className={styles.decoration1}
         />
       </div>
--- src/Components/Sections/Inicio/Quote/index.tsx
+++ src/Components/Sections/Inicio/Quote/index.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
 
 export function Quote() {
-  return (<div>
-    <p className={styles.quoteText}>“Quem olha para fora sonha, quem olha para dentro desperta.”</p>
-    <h3 className={styles.quoteAuthor}>Carl Jung</h3>
-  </div>)
+  return (
+    <div>
+      <p className={styles.quoteText}>“Quem olha para fora sonha, quem olha para dentro desperta.”</p>
+      <p className={styles.quoteAuthor}>Carl Jung</p>
+    </div>
+  )
 }
