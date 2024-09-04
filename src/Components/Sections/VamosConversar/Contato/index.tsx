cd81c9e5635e27e215143259db9e94d43f1390d4
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-09-04 11:37:59 -0300

    added whatsapp web redirection on contact number

--- src/Components/Sections/VamosConversar/Contato/index.tsx
+++ src/Components/Sections/VamosConversar/Contato/index.tsx
@@ -5,9 +5,14 @@ export function Contato() {
   return (
     <div className={styles.contact}>
       <h1 className={styles.contactTitle}>Vamos conversar?</h1>
-      <div className={styles.contactInfo}>
-        <i className='bx bxl-whatsapp'></i>
-        <span className={styles.contactNumber}>31 98400 - 3796</span>
+      <div className={styles.contactInfoContainer}>
+        <a className={styles.contactInfo} 
+          href='https://wa.me/5531988187830?text=Ol%C3%A1%21+Gostaria+de+agendar+um+atendimento'
+          target='_blank'
+        >
+          <i className='bx bxl-whatsapp'></i>
+          <span className={styles.contactNumber}>31 98400 - 3796</span>
+        </a>
       </div>
       <Image
         src="/logo2.png"
