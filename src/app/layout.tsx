4dedd804d98dd1e2af0ebe115eba3d68cb2ee684
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-09-13 20:11:02 -0300

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,21 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&display=swap" rel="stylesheet"></link>
         <meta name = "format-detection" content = "telephone=no"></meta>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16695713159"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-16695713159');
+          `}
+        </script>
+        <script>
+          {`
+            gtag('event', 'conversion', {'send_to': 'AW-16695713159/Nnw6CJfE8dAZEIe7kZk-'});
+          `}
+        </script>
       </head>
       <body>{children}</body>
     </html>
