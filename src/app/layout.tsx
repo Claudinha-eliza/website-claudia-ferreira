f9a141913a25b7ad5b5d1c5dda76f1c9dd004cb9
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-09-13 21:18:51 -0300

    google tag attempt 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-page-custom-font */
+import { GoogleTagManager  } from '@next/third-parties/google'
 import "./globals.css";
 
 export default function RootLayout({
@@ -16,22 +17,11 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&display=swap" rel="stylesheet"></link>
         <meta name = "format-detection" content = "telephone=no"></meta>
-        <script>
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T7M25X9J');
-          `}
-        </script>
       </head>
       <body>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7M25X9J" height="0" width="0"></iframe>
-        </noscript>
         {children}
       </body>
+      <GoogleTagManager gtmId='GTM-TM92P8FF' />
     </html>
   );
 }
