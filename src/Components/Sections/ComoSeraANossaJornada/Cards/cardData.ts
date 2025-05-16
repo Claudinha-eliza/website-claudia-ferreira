09786822bebfd1ccc4eec264a36baa27bbae47e0
Vittor-Javidan <vittor.javidan.t@gmail.com>
2025-05-16 16:25:00 -0300

    0.1.1 Atualização de textos

--- src/Components/Sections/ComoSeraANossaJornada/Cards/cardData.ts
+++ src/Components/Sections/ComoSeraANossaJornada/Cards/cardData.ts
@@ -1,22 +1,22 @@
 export const cardData: { title:string, text:string }[] = [
   {
     title: 'Estabelecimento de vínculo',
-    text: 'O psicanalista trabalha para estabelecer uma relação de confiança com a criança ou adolescente. Isso é fundamental para que eles se sintam confortáveis para expressarem-se.',
+    text: 'O psicanalista trabalha para estabelecer uma relação de confiança com a criança ou adolescente. Isso é fundamental para que eles se sintam confortáveis para se expressar.',
   },
   {
     title: 'Comunicação não verbal',
-    text: 'Como crianças e adolescentes podem ter dificuldade para se articular em palavras, o psicanalista presta atenção às comunicações não verbais, como brincadeiras, desenhos, jogos e comportamentos.',
+    text: 'Como crianças e adolescentes podem ter dificuldade para se articular em palavras, o psicanalista presta atenção e intervém sobre as comunicações não verbais, como brincadeiras, desenhos, jogos e comportamentos.',
   },
   {
     title: 'Brincadeiras',
-    text: 'Elas merecem um tópico à parte! Em vez de apenas conversas diretas, a psicanálise com crianças muitas vezes utiliza as brincadeiras como forma de comunicação simbólica, pois elas podem revelar muito sobre os sentimentos e as preocupações da criança e do adolescente.',
+    text: 'Elas merecem um tópico à parte! A psicanálise com crianças e adolescentes muitas vezes utiliza as brincadeiras como forma de comunicação simbólica, pois elas podem revelar muito sobre o psiquismo desses indivíduos.',
   },
   {
     title: 'Análise dos conflitos e fantasias',
-    text: 'Assim como na psicanálise de adultos, o foco está na abordagem dos conflitos internos e fantasias inconscientes da criança ou adolescente. Isso pode incluir por exemplo preocupações com a separação dos pais, medos, desejos e experiências traumáticas.',
+    text: 'Assim como na psicanálise de adultos, o foco está na abordagem dos conflitos internos e fantasias inconscientes da criança ou adolescente.',
   },
   {
     title: 'Envolvimento dos pais',
-    text: 'Os pais ou responsáveis desempenham um papel importante no processo psicanalítico com crianças e adolescentes. Eles são eventualmente envolvidos no tratamento para discutir questões ou dinâmicas familiares e estratégias para apoiar o desenvolvimento emocional dos filhos.',
+    text: 'Os pais ou responsáveis desempenham um papel importante no processo psicanalítico com crianças e adolescentes. Eles são eventualmente envolvidos no tratamento para analisar questões ou dinâmicas familiares e maneiras de apoiar o desenvolvimento emocional dos filhos.',
   },
 ]
--- src/Components/Sections/ComoSeraANossaJornada/TopText/index.tsx
+++ src/Components/Sections/ComoSeraANossaJornada/TopText/index.tsx
@@ -5,8 +5,8 @@ export function TopText() {
     <div>
       <h1 className={styles.title}>Como será a nossa jornada</h1>
       <p className={styles.text1}>A psicanálise, desenvolvida por Sigmund Freud no início do século XX, que evolui desde então com as contribuições de muitos outros teóricos, é um campo de estudo que busca compreender o funcionamento da subjetividade humana; especialmente os pensamentos, sentimentos e comportamentos que podem não ser completamente conscientes, porque são influenciados por impulsos inconscientes, desejos, fantasias e experiências reprimidas.</p>
-      <p className={styles.text2}>Essencialmente, a psicanálise ajuda os pacientes a explorar as camadas mais profundas da mente através da fala, visando promover autoconhecimento, crescimento pessoal e alívio de sintomas psicológicos.</p>
-      <p className={styles.text3}>Com crianças e adolescentes adapta-se às necessidades específicas desses indivíduos, levando em consideração o momento de desenvolvimento em que estão. Assim, alguns aspectos ganham relevância:</p>
+      <p className={styles.text2}>Essencialmente, a psicanálise ajuda os pacientes a explorar as camadas mais profundas da mente através da fala, visando promover autoconhecimento e alívio de sintomas psicológicos.</p>
+      <p className={styles.text3}>Com crianças e adolescentes, adapta-se às necessidades específicas desses indivíduos, levando em consideração o momento de desenvolvimento em que estão. Assim, alguns aspectos ganham relevância:</p>
     </div>
   )
 }
